c0a8be18a0d71422f3cbfc704f68e528c1d5de5e
Anastasia Malets <90954585+Anastasunny@users.noreply.github.com>
2021-11-16 18:39:14 +0300

    Add files via upload

---
+++ src/wd.js
@@ -0,0 +1,28 @@
+const {Builder, By} = require('selenium-webdriver');
+require('chromedriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+
+  await driver.get('https://chromedriver.chromium.org/')
+  await driver.sleep(5000);
+
+  const title1 = await driver.findElement(By.css('.Rn3Z1b'));
+  let text = await title1.getText();
+  console.log(text);
+
+  const headerItems = await driver.findElements(By.css('.plFg0c .PsKE7e'));
+  await headerItems[2].click();
+  await driver.sleep(5000);
+
+  const title2 = await driver.findElement(By.css('.Rn3Z1b'));
+  driver.executeScript("arguments[0].style.backgroundColor = 'red'", title2);
+
+  text = await title2.getText();
+  console.log(text);
+
+  await driver.sleep(5000);
+  await driver.quit();
+  
+})();
+
